c25cfadc85927b928296eb45ceaa8543adab905f
OM BHATT <98407757+21IT008@users.noreply.github.com>
2023-10-18 22:58:33 +0530

    Add files via upload

---
+++ E-Farm/frontend/src/utils/constants.js
@@ -0,0 +1,161 @@
+// Categories Used In Entire App
+import p1 from "../assets/images/Categories/10.jpeg";
+import p2 from "../assets/images/Categories/11.jpg";
+import p3 from "../assets/images/Categories/12.jpeg";
+import p4 from "../assets/images/Categories/13.jpeg";
+import p5 from "../assets/images/Categories/14.jpeg";
+import p6 from "../assets/images/Categories/15.jpeg";
+import p7 from "../assets/images/Categories/16.jpeg";
+import p8 from "../assets/images/Categories/17.jpeg";
+import p9 from "../assets/images/Categories/18.jpeg";
+import p10 from "../assets/images/Categories/19.jpeg";
+import p11 from "../assets/images/Categories/20.jpeg";
+import p12 from "../assets/images/Categories/21.jpeg";
+import p13 from "../assets/images/Categories/22.jpeg";
+import p14 from "../assets/images/Categories/23.jpeg";
+import p15 from "../assets/images/Categories/24.jpeg";
+import p16 from "../assets/images/Categories/25.jpeg";
+import p17 from "../assets/images/Categories/26.webp";
+import p18 from "../assets/images/Categories/27.jpeg";
+import p19 from "../assets/images/Categories/28.jpeg";
+import p20 from "../assets/images/Categories/29.jpeg";
+import p21 from "../assets/images/Categories/30.jpg";
+import p22 from "../assets/images/Categories/31.webp";
+
+export const categories = ["Grains", "Pulses", "Vegetables", "Fruits"];
+
+// Product Sliders Offers
+export const offerProducts = [
+  {
+    image: p1,
+    name: "Wheat",
+    offer: "From ₹3,290",
+    tag: "Made In India Brands",
+  },
+  {
+    image: p2,
+    name: "Bajari",
+    offer: "In Focus Now",
+    tag: "Buy Now!",
+  },
+  {
+    image: p3,
+    name: "Jowar",
+    offer: "Min 70% Off",
+    tag: "Buy Now!",
+  },
+  {
+    image: p4,
+    name: "Corn",
+    offer: "Min 40% Off",
+    tag: "Grab Now!",
+  },
+  {
+    image: p5,
+    name: "Carrot",
+    offer: "Min 40% Off",
+    tag: "Discover Now!",
+  },
+  {
+    image: p6,
+    name: "Tomato",
+    offer: "Min 50% Off",
+    tag: "Great Savings!",
+  },
+  {
+    image: p7,
+    name: "Chilly",
+    offer: "Upto 40% Off",
+    tag: "Buy Now!",
+  },
+  {
+    image: p8,
+    name: "Potato",
+    offer: "Upto 50% Off",
+    tag: "Great Savings!",
+  },
+  {
+    image: p9,
+    name: "Bringles",
+    offer: "Min 60% Off",
+    tag: "Grab Now!",
+  },
+  {
+    image: p10,
+    name: "Apples",
+    offer: "Upto 30% Off",
+    tag: "Discover Now!",
+  },
+  {
+    image: p11,
+    name: "Grapes",
+    offer: "Min 20% Off",
+    tag: "Shop Now!",
+  },
+  {
+    image: p12,
+    name: "Banana",
+    offer: "Upto 75% Off",
+    tag: "Grab Now!",
+  },
+  {
+    image: p13,
+    name: "Guava",
+    offer: "Min 50% Off",
+    tag: "Explore Now!",
+  },
+  {
+    image: p14,
+    name: "Mango",
+    offer: "Min 50% Off",
+    tag: "Great Savings!",
+  },
+  {
+    image: p15,
+    name: "Dragon Fruit",
+    offer: "Upto 60% Off",
+    tag: "Buy Now!",
+  },
+  {
+    image: p16,
+    name: "Groundnut",
+    offer: "Upto 80% Off",
+    tag: "Grab Now!",
+  },
+  {
+    image: p17,
+    name: "Tuvar",
+    offer: "Upto 55% Off",
+    tag: "Top Rated",
+  },
+  {
+    image: p18,
+    name: "Mug",
+    offer: "Min 60% Off",
+    tag: "Great Savings!",
+  },
+  {
+    image: p19,
+    name: "Udad",
+    offer: "Upto 65% Off",
+    tag: "Discover Now!",
+  },
+  {
+    image: p20,
+    name: "Chana",
+    offer: "From ₹1,900",
+    tag: "Relax & Work",
+  },
+  {
+    image: p21,
+    name: "Cotton",
+    offer: "Min 50% Off",
+    tag: "Explore Now!",
+  },
+  {
+    image: p22,
+    name: "Dolichos Seeds",
+    offer: "Upto 20% Off",
+    tag: "Shop Now!",
+  },
+];
